8d684bfdb450b36b11ed8a222619a3e8dc09ff06
paulohenriquedev <paulohenrique.root@gmail.com>
2025-11-13 17:38:44 -0300

    feat: create exercise 2

---
+++ 01-introdução/variaveis.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var inteiro int = 12
+	var flutuante float64 = 3.14
+	var booleano bool = true
+	var string string = "Paulo Henrique"
+
+	fmt.Printf(" Inteiro: %d \n", inteiro)
+	fmt.Printf(" Flutuante: %.2f \n", flutuante)
+	fmt.Printf(" Booleano: %v \n", booleano)
+	fmt.Printf(" String: %v \n", string)
+
+}
